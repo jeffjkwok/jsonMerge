e63123886ee7704b532016fb131d27aebd8e61dc
Jeff Kwok <jeffjkwok@gmail.com>
2018-02-08 08:03:40 -0800

    device file creation, start of sort

--- mergeJSON.py
+++ mergeJSON.py
@@ -1,7 +1,9 @@
 import json, os, os.path, glob;
+from datetime import datetime
 
 # length = len(os.listdir('./data'));
 allData = [];
+devDict = {};
 
 jsonLength = len(glob.glob1("./data", "*.json"))
 print jsonLength
@@ -10,11 +12,20 @@ for x in xrange (1, jsonLength + 1):
     path = "./data/JSON" + str(x) + ".json"
     with open(path) as json_data:
         data = json.load(json_data)
+        # creates key in dictionary for each device
+        
+        if data['SerialNumber'] not in devDict:
+            time = data['TimeCreated'][:-6]
+            print datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%f')
+            
+            devDict[data['SerialNumber']] = [data];
+        else: 
+            devDict[data['SerialNumber']].append(data);
         allData.append(data)
 
-# print allData
+# for each serial number in the dictionary it creates a file with the associated impacts
+for key in devDict:
+    f = open(key + '.json', "w+")
+    f.write(json.dumps(devDict[key], ensure_ascii=False));
+    f.close();
 print len(allData)
-
-# with open('1.json') as json_data:
-#     d = json.load(json_data)
-#     print (d)
