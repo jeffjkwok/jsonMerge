16bb42af6f54db2a1581e6cacad14fffb8545ec6
Jeff Kwok <jkwok@athleteintelligence.com>
2018-02-07 16:06:33 -0800

    copied data, organization

--- mergeJSON.py
+++ mergeJSON.py
@@ -1,6 +1,11 @@
 import json, os, os.path;
 
-print len(os.listdir('.'));
+length = len(os.listdir('.'));
+allData = [];
+print length
+for x in xrange (1, length + 1):
+    print x
+
 
 with open('1.json') as json_data:
     d = json.load(json_data)
